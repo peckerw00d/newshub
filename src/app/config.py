d00b63cf6d296966b452442f6590e760e67d42c0
inal <balkarov05@gmail.com>
2025-06-09 12:53:16 +0300

    added config

---
+++ src/app/config.py
@@ -0,0 +1,16 @@
+from os import getenv
+
+
+from pydantic import BaseModel, Field
+
+
+class PostgresConfig(BaseModel):
+    url: str = str(getenv("POSTGRES_URL"))
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 50
+    max_overflow: int = 10
+
+
+class Config(BaseModel):
+    postgres: PostgresConfig = Field(default_factory=lambda: PostgresConfig())
