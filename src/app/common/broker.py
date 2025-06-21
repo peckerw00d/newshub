d1b3048c67373247fb67658e9f5909f4dbcadf22
inal <balkarov05@gmail.com>
2025-06-21 10:27:42 +0300

    init taskiq scheduler

--- src/app/common/broker.py
+++ src/app/common/broker.py
@@ -1,4 +1,6 @@
 from dishka.integrations.taskiq import setup_dishka
+from taskiq import TaskiqScheduler
+from taskiq.schedule_sources import LabelScheduleSource
 from taskiq_aio_pika import AioPikaBroker
 
 from src.app.services.tasks.tasks import register_tasks
@@ -6,11 +8,15 @@ from src.app.common.di.setup import setup_ioc_container
 from src.app.common.config import Config
 
 
-def configure_broker(config: Config, broker: AioPikaBroker) -> None:
-    register_tasks(broker=broker)
+def create_scheduler(broker: AioPikaBroker):
+    return TaskiqScheduler(broker, sources=[LabelScheduleSource(broker)])
 
-    container = setup_ioc_container(config=config, broker=broker)
-    setup_dishka(container=container, broker=broker)
+
+def configure_broker(config: Config, broker: AioPikaBroker) -> None:
+    register_tasks(broker)
+
+    container = setup_ioc_container(config, broker)
+    setup_dishka(container, broker)
 
 
 def setup_taskiq_broker(config: Config) -> AioPikaBroker:
@@ -18,3 +24,10 @@ def setup_taskiq_broker(config: Config) -> AioPikaBroker:
     configure_broker(config, broker)
 
     return broker
+
+
+def setup_taskiq_scheduler(config: Config) -> TaskiqScheduler:
+    broker = AioPikaBroker(config.rabbitmq.url)
+    scheduler = create_scheduler(broker)
+
+    return scheduler
